ea0aeee9431fbde766935e80d69f6a4d8353e36c
Stellarium Foundation <stellarium.foundation.us@gmail.com>
2026-02-21 18:47:06 +0000

    fix: correct latlong2 import entry point

--- lib/party.dart
+++ lib/party.dart
@@ -11,7 +11,7 @@ import 'websocket.dart';
 import 'constants.dart';
 
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong2.dart';
+import 'package:latlong2/latlong.dart';
 
 class CreatePartyScreen extends ConsumerStatefulWidget {
   const CreatePartyScreen({super.key});
